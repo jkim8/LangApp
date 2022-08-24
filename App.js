260584c8f1a5042bdca9c3dcd1f68fec6e210811
jkim8 <juno0218@naver.com>
2022-08-24 15:48:12 -0400

    ValueXY

--- App.js
+++ App.js
@@ -17,19 +17,27 @@ const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
   const [up, setUp] = useState(true);
-  const Y_POSITION = useRef(new Animated.Value(300)).current;
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 300 })).current;
   const toggleUp = () => setUp((prev) => !prev);
   const moveUp = () => {
-    Animated.timing(Y_POSITION, {
+    Animated.timing(position, {
       toValue: up ? 300 : -300,
-      useNativeDriver: true,
+      useNativeDriver: false,
       duration: 1000,
     }).start(toggleUp);
   };
-
-  const borderRadius = Y_POSITION.interpolate({
+  const rotation = position.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["-360deg", "360deg"],
+  });
+  const borderRadius = position.y.interpolate({
     inputRange: [-300, -100, 0, 100, 300],
     outputRange: [0, 100, 0, 100, 0],
+  });
+
+  const bgColor = position.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
   });
 
   return (
@@ -38,7 +46,8 @@ export default function App() {
         <AnimatedBox
           style={{
             borderRadius,
-            transform: [{ translateY: Y_POSITION }],
+            backgroundColor: bgColor,
+            transform: [{ rotateY: rotation }, { translateY: position.y }],
           }}
         />
       </Pressable>
