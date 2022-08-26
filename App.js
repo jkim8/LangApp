513dc57f05565e7ce05a27b99abfee5494cf81fd
jkim8 <juno0218@naver.com>
2022-08-25 22:24:38 -0400

    onPanResponderRelease

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Animated, Dimensions, PanResponder } from "react-native";
+import React, { useRef } from "react";
+import { Animated, PanResponder } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -12,42 +12,44 @@ const Box = styled.View`
   width: 200px;
   height: 200px;
 `;
-
 const AnimatedBox = Animated.createAnimatedComponent(Box);
-const { width: SCREEN_WIDTH, height: SCREEN_HIGHT } = Dimensions.get("window");
+
 export default function App() {
-  const position = useRef(
+  const POSITION = useRef(
     new Animated.ValueXY({
       x: 0,
       y: 0,
     })
   ).current;
-
-  const borderRadius = position.y.interpolate({
-    inputRange: [-300, -100, 0, 100, 300],
-    outputRange: [45, 100, 45, 100, 45],
-  });
-
-  const bgColor = position.y.interpolate({
+  const borderRadius = POSITION.y.interpolate({
     inputRange: [-300, 300],
-    outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
+    outputRange: [100, 0],
   });
-
+  const bgColor = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"],
+  });
   const panResponder = useRef(
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (_, { dx, dy }) => {
-        position.setValue({
+        POSITION.setValue({
           x: dx,
           y: dy,
         });
       },
       onPanResponderRelease: () => {
-        position.setValue({ x: 0, y: 0 });
+        Animated.spring(POSITION, {
+          toValue: {
+            x: 0,
+            y: 0,
+          },
+          bounciness: 20,
+          useNativeDriver: false,
+        }).start();
       },
     })
   ).current;
-
   return (
     <Container>
       <AnimatedBox
@@ -55,7 +57,7 @@ export default function App() {
         style={{
           borderRadius,
           backgroundColor: bgColor,
-          transform: position.getTranslateTransform(),
+          transform: POSITION.getTranslateTransform(),
         }}
       />
     </Container>
